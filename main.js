c1f90fead09ede9350b692c416f273895d4da901
Пользователь <BeknazarBolotbekowv>
2022-11-24 13:41:56 +0600

    lesson 8

---
+++ main.js
@@ -0,0 +1,15 @@
+// * Тема 
+
+// * git, github
+// * setTimeout, setInterval
+// * Project - Todo List
+
+//----------
+
+setTimeout (() => {
+  console.log('setTimout')
+}, 3000)
+
+setInterval (() => {
+  console.log('setInterval')
+}, 2000)
